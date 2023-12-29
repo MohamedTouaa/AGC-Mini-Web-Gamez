edea84a431e6ecefb35f71402a47b4b2223ff380
Haru <96742478+HaruHiiru@users.noreply.github.com>
2023-12-29 16:01:10 +0100

    dialogue system w ta7ya brackeys

---
+++ Assets/Haru/DialogueManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI dialogueText;
+
+    public Animator animator;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(ObjectDialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) 
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence(string sentence)
+    {
+        dialogueText.text = "";
+
+        foreach(char letter in sentence.ToCharArray())    
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.02f); ;
+        }
+    }
+    public void EndDialogue() 
+    {
+        Debug.Log("end of convo");
+        animator.SetBool("IsOpen", false);
+
+    }
+}
